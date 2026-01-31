b3edc54a7807694fe81e3a3a85d5d915ccf2f49f
Shani <nadeeshaniwijebandara2000@gmail.com>
2026-01-31 14:46:33 +0530

    Fix Footer import path for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./pages/Home";
 import Chatbot from "./Components/Chatbot";
 
